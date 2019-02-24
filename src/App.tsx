c271299bff55e67f87a3541a9a43969920d14016
dontry <mccoy018@gmail.com>
2019-02-24 16:07:16 +1100

    feat(Router): add react-router & react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import Counter from "./components/counter";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { setConfig } from "react-hot-loader";
 import MobxReactDevtools from "mobx-react-devtools";
 import { FieldInput, FieldState } from "./components/FieldInput";
@@ -13,32 +13,9 @@ setConfig({ ignoreSFC: true });
 type Props = { message: string };
 
 const App: React.FC<Props> = ({ message }) => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h3>{message}</h3>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-    <main>
-      <Counter />
-      <br />
-      <FieldInput fieldState={appState.currentItem} />
-      <button onClick={appState.addCurrentItem}>Add Item</button>
-      <ul>
-        {appState.items.map((item: string) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-      <MobxReactDevtools />
-    </main>
-  </div>
+  <Router>
+    <Route exact path="/" component={Counter} />
+  </Router>
 );
 
 export default observer(App);
